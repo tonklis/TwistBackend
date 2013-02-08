d40819ce6fcebf479f08995678c03aba4fb35f81
tonklis <benjamin.hernandez@in2teck.com>
2013-02-07 22:12:31 -0600

    Login test

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,15 +2,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	
 	def facebook
 		@user = User.find_for_facebook_oauth(auth_hash, current_user)
+		persisted = false
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
+      #sign_in_and_redirect @user, :event => :authentication
+			persisted = true
     else
       session["devise.facebook_data"] = auth_hash.except("extra")
-      redirect_to signup_url(@user)
+      #redirect_to signup_url(@user)
     end
 		
+		respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: persisted }
+    end
+
   end
 
 	def auth_hash
