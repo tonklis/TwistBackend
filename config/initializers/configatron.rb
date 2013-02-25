36719d8af87c0da9f4564635a45d67424d702257
tonklis <benjamin.hernandez@in2teck.com>
2013-02-25 00:40:19 -0600

    Forced development certificate for heroku

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -5,3 +5,22 @@ configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
 configatron.gcm_on_rails.api_key = 'AIzaSyB8DMKpnr0KG-dgWFY9DDnzrJ2_iEVPulQ'
 configatron.gcm_on_rails.app_name = 'org.in2teck.codice.AdivinaMe'
 configatron.gcm_on_rails.delivery_format = 'json'
+
+# PROD ENVIRONMENT
+#configatron.apn.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_production.pem"
+#configatron.apn.feedback.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_production.pem"
+#configatron.apn.feedback.host = "feedback.push.apple.com"
+#configatron.apn.feedback.passphrase = ""
+#configatron.apn.feedback.port = 2196
+#configatron.apn.host = "gateway.push.apple.com"
+#configatron.apn.passphrase = ""
+#configatron.apn.port = 2195
+
+configatron.apn.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_development.pem"
+configatron.apn.feedback.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_development.pem"
+configatron.apn.feedback.host = "feedback.sandbox.push.apple.com"
+configatron.apn.feedback.passphrase = ""
+configatron.apn.feedback.port = 2196
+configatron.apn.host = "gateway.sandbox.push.apple.com"
+configatron.apn.passphrase = 
+configatron.apn.port = 2195
--- db/schema.rb
+++ db/schema.rb
@@ -132,4 +132,3 @@ ActiveRecord::Schema.define(:version => 20130222205535) do
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
-
