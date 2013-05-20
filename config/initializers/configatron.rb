4908a26d462c19284e9cb08375c569dc4a7be09b
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-05-20 18:13:36 -0500

    Test for production certificate notifications

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -7,20 +7,20 @@ configatron.gcm_on_rails.app_name = 'org.in2teck.codice.AdivinaMe'
 configatron.gcm_on_rails.delivery_format = 'json'
 
 # PROD ENVIRONMENT
-#configatron.apn.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_production.pem"
-#configatron.apn.feedback.cert = "/Users/gina/git/twist/TwistBack/config/apple_push_notification_production.pem"
-#configatron.apn.feedback.host = "feedback.push.apple.com"
-#configatron.apn.feedback.passphrase = ""
-#configatron.apn.feedback.port = 2196
-#configatron.apn.host = "gateway.push.apple.com"
-#configatron.apn.passphrase = ""
-#configatron.apn.port = 2195
-
-configatron.apn.cert = "config/apple_push_notification_development.pem"
-configatron.apn.feedback.cert = "config/apple_push_notification_development.pem"
-configatron.apn.feedback.host = "feedback.sandbox.push.apple.com"
+configatron.apn.cert = "config/apple_push_notification_production.pem"
+configatron.apn.feedback.cert = "config/apple_push_notification_production.pem"
+configatron.apn.feedback.host = "feedback.push.apple.com"
 configatron.apn.feedback.passphrase = ""
 configatron.apn.feedback.port = 2196
-configatron.apn.host = "gateway.sandbox.push.apple.com"
+configatron.apn.host = "gateway.push.apple.com"
 configatron.apn.passphrase = ""
 configatron.apn.port = 2195
+
+#configatron.apn.cert = "config/apple_push_notification_development.pem"
+#configatron.apn.feedback.cert = "config/apple_push_notification_development.pem"
+#configatron.apn.feedback.host = "feedback.sandbox.push.apple.com"
+#configatron.apn.feedback.passphrase = ""
+#configatron.apn.feedback.port = 2196
+#configatron.apn.host = "gateway.sandbox.push.apple.com"
+#configatron.apn.passphrase = ""
+#configatron.apn.port = 2195
